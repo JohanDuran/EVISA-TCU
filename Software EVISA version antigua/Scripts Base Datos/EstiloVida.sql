2cdba58264dd8cb11fe820bfcc811167bf867303
johanduran <johan.durancerdas@gmail.com>
2017-07-25 21:21:04 -0600

    first ER

---
+++ Software EVISA version antigua/Scripts Base Datos/EstiloVida.sql
@@ -0,0 +1,41 @@
+﻿-- Table: public."EstiloVida"
+
+-- DROP TABLE public."EstiloVida";
+
+CREATE TABLE public."EstiloVida"
+(
+  nombre character varying(20) NOT NULL,
+  apellido1 character varying(20) NOT NULL,
+  apellido2 character varying(20) NOT NULL,
+  anno integer NOT NULL,
+  iniciofin character(1) NOT NULL,
+  medicamentos boolean,
+  ejercicio boolean,
+  constancia character varying(15),
+  accesible boolean,
+  apetito character varying(10),
+  desayuno boolean,
+  meriendamanana boolean,
+  almuerzo boolean,
+  meriendatarde boolean,
+  cena boolean,
+  meriendanoche boolean,
+  frecuenciaembutidos character varying(15),
+  frecuenciahuevos character varying(15),
+  frecuenciavegetales character varying(15),
+  frecuenciaensaladas character varying(15),
+  frecuenciafrutas character varying(15),
+  frecuenciagaseosas character varying(15),
+  frecuenciareposteria character varying(15),
+  frecuenciarapidas character varying(15),
+  fuma boolean,
+  CONSTRAINT "EstiloVida_pkey" PRIMARY KEY (nombre, apellido1, apellido2, anno, iniciofin),
+  CONSTRAINT "EstiloVida_nombre_apellido1_apellido2_anno_inicioFin_fkey" FOREIGN KEY (nombre, apellido1, apellido2, anno, iniciofin)
+      REFERENCES public."Persona" (nombre, apellido1, apellido2, anno, iniciofin) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."EstiloVida"
+  OWNER TO postgres;
---
+++ Software EVISA version antigua/Scripts Base Datos/Persona.sql
@@ -0,0 +1,36 @@
+﻿-- Table: public."Persona"
+
+-- DROP TABLE public."Persona";
+
+CREATE TABLE public."Persona"
+(
+  nombre character varying(20) NOT NULL,
+  apellido1 character varying(20) NOT NULL,
+  apellido2 character varying(20) NOT NULL,
+  anno integer NOT NULL,
+  iniciofin character(1) NOT NULL,
+  correo character varying(30),
+  ocupacion character varying(20),
+  residencia character varying(50),
+  edad integer,
+  niveleducacion character varying(30),
+  telefono integer,
+  cantpersonasvive integer,
+  talla real,
+  peso real,
+  imc real,
+  clasificacion character varying(20),
+  cc real,
+  porcmasagrasa real,
+  porcagua real,
+  masamuscular real,
+  masaosea real,
+  grasavisceral integer,
+  edadmetabolica integer,
+  CONSTRAINT "Persona_pkey" PRIMARY KEY (nombre, apellido1, apellido2, anno, iniciofin)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."Persona"
+  OWNER TO postgres;
